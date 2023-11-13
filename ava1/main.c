6a42c26f9b9c33fc809cc7bc59493a10b98a4ce7
Jamilly da Silva Pinheiro <jamilly.p11@aluno.ifsc.edu.br>
2023-11-13 11:20:27 -0300

    passada as funções do main.c para o processamento.h

--- ava1/main.c
+++ ava1/main.c
@@ -1,47 +1,7 @@
 #include "processamento.h"
-#include <stdio.h>
-#include <sys/time.h>
-#include <time.h>
-#include <string.h>
+
 //função que vai ler e separar os dados de login, senha e nome de cada usuario
-void separa_usu(FILE *arquivo, struct usuario *usuario) {
-    //separo os login
-    fgets(usuario->login, sizeof(usuario->login), arquivo);
-    //"pula" uma posição
-    fgetc(arquivo);
-    //separo as senhas
-    fgets(usuario->senha, sizeof(usuario->senha), arquivo);
-    fgetc(arquivo);
-    //separo os nomes
-    fgets(usuario->nome, sizeof(usuario->nome), arquivo);
-    fgetc(arquivo);
 
-    //sizeof é o responsavel por saber o tamanho se caracteres
-    //coloquei um fgetc logo após o fgets pois ao debugar o fgets voltava uma pocição para trás
-}
-
-
-// Busca o usuario
-int bucar_usu(struct usuario *usuarios, char *login, char *senha) {
-    //Percorro o vetor usarios
-    for (int i = 0; i < 500; i++) {
-
-        //comparo se o login atual e passado via teclado são iguais
-        if (strcmp(usuarios[i].login, login) == 0) {
-
-            //comparo se o login atual e passado via teclado são iguais
-            if (strcmp(usuarios[i].senha, senha) == 0) {
-                return i;
-            }
-        }
-    }
-    // O usuário não foi encontrado
-    return 0;
-}
-//imprimi boas vindas ao usuario
-void hello(struct usuario *usuario) {
-    printf("Ola, %s!Seja bem vindo.\n", usuario->nome);
-}
 
 int main(int argc, char **argv) {
     //abertura do arquivo
--- ava1/processamento.h
+++ ava1/processamento.h
@@ -2,9 +2,11 @@
 #define AVA1_PROCESSAMENTO_C
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/time.h>
 #include <time.h>
+#include <string.h>
+
+
 
 
 struct usuario{
@@ -20,5 +22,43 @@ struct usuario{
 #include <unistd.h>
 #endif
 
+void separa_usu(FILE *arquivo, struct usuario *usuario) {
+    //separo os login
+    fgets(usuario->login, sizeof(usuario->login), arquivo);
+    //"pula" uma posição
+    fgetc(arquivo);
+    //separo as senhas
+    fgets(usuario->senha, sizeof(usuario->senha), arquivo);
+    fgetc(arquivo);
+    //separo os nomes
+    fgets(usuario->nome, sizeof(usuario->nome), arquivo);
+    fgetc(arquivo);
+
+    //sizeof é o responsavel por saber o tamanho se caracteres
+    //coloquei um fgetc logo após o fgets pois ao debugar o fgets voltava uma pocição para trás
+}
+
+
+// Busca o usuario
+int bucar_usu(struct usuario *usuarios, char *login, char *senha) {
+    //Percorro o vetor usarios
+    for (int i = 0; i < 500; i++) {
+
+        //comparo se o login atual e passado via teclado são iguais
+        if (strcmp(usuarios[i].login, login) == 0) {
+
+            //comparo se o login atual e passado via teclado são iguais
+            if (strcmp(usuarios[i].senha, senha) == 0) {
+                return i;
+            }
+        }
+    }
+    // O usuário não foi encontrado
+    return 0;
+}
+//imprimi boas vindas ao usuario
+void hello(struct usuario *usuario) {
+    printf("Ola, %s!Seja bem vindo.\n", usuario->nome);
+}
 
 #endif
